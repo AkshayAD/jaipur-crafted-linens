42a41e0b2c2db3f207a772b2a78d0897e81460b7
AkshayAD <akshaydewalwar305@gmail.com>
2025-02-15 18:04:11 +0530

    Create 360view.js

---
+++ assets/js/360view.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const viewer = document.getElementById('viewer');
+  if (viewer) {
+    viewer.innerHTML = '<p>360 View functionality coming soon.</p>';
+  }
+});
